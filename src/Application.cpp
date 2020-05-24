343028b4030eca4294174e7ecc8fa400936fa44c
Colin Harker <colinharker5@yahoo.com>
2020-05-24 15:10:34 -0400

    Update Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,6 +8,23 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError(){
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] " << error << ": " << function << " " << file << " " << line << '\n';
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -158,8 +175,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+       
        /* glColor3f(1, 0, 0);
         GLUquadric* quad;
         quad = gluNewQuadric();
